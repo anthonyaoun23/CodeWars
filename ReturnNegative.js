e77abc416c19506071275af39bc20f79894383b5
Anthony Aoun <anthony.aoun1@hotmail.com>
2018-11-13 14:13:27 -0500

    adding completed challenge

---
+++ ReturnNegative.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function makeNegative(num) {
+  if (num <= 0) return num;
+  return num * -1;
+}
+
+// function makeNegative(num) {
+//     return -Math.abs(num);
+// }
+
+// function makeNegative(num) {
+//     return num < 0 ? num : -num;
+// }
+
+// makeNegative = n => -Math.abs(n)
+
+// function makeNegative(num) {
+//     return num > 0 ? -num : num;
+// }
